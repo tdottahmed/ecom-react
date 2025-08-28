e7b5e047c2e58381a7ab9b57eafcfcd52c5a24c6
Mojahidul Islam <itsemon245@gmail.com>
2025-08-28 14:41:00 +0600

    add: Seeder Generator

---
+++ config/courier-fraud-checker-bd.php
@@ -0,0 +1,20 @@
+<?php 
+
+return [
+    'pathao' => [
+        'user' => env('PATHAO_USER'),
+        'password' => env('PATHAO_PASSWORD'),
+    ],
+
+    'redx' => [
+        'phone' => env('REDX_PHONE'),
+        'password' => env('REDX_PASSWORD'),
+    ],
+
+    'steadfast' => [
+        'user' => env('STEADFAST_USER'),
+        'password' => env('STEADFAST_PASSWORD'),
+    ],
+];
+
+?>
